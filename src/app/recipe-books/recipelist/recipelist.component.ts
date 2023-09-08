349f191326791f3f6f6b4495dcd28dd15ce52475
laurence564 <laurence-plaskitt@outlook.com>
2023-09-08 12:30:12 +0100

    Adding recipes list.

--- src/app/recipe-books/recipelist/recipelist.component.ts
+++ src/app/recipe-books/recipelist/recipelist.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Recipe} from "../recipe/recipe.model";
 
 @Component({
   selector: 'app-recipelist',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipelist.component.css']
 })
 export class RecipelistComponent {
+
+  public recipes: Recipe[] = [
+    new Recipe(
+      'Fish & Chips',
+      'A British Classic!',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Fish%2C_chips_and_mushy_peas.jpg/800px-Fish%2C_chips_and_mushy_peas.jpg?20070522095400'
+    ),
+    new Recipe(
+      'Chicken Tikka Masala',
+      'A British Classic!',
+      'https://images.pexels.com/photos/14537690/pexels-photo-14537690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    )
+  ];
 
 }
