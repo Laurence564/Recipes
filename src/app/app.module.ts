b203b4b6a48a31da18d0f871bc23369cbddc7e80
laurence564 <laurence-plaskitt@outlook.com>
2023-09-08 10:26:04 +0100

    Added core components for the app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-lists/shopping-list/shopping-list.component';
+import { EditshoppinglistComponent } from './shopping-lists/editshoppinglist/editshoppinglist.component';
+import { HeaderComponent} from "./header/header.component";
+import { RecipelistComponent } from './recipe-books/recipelist/recipelist.component';
+import { RecipedetailComponent } from './recipe-books/recipedetail/recipedetail.component';
+import { RecipeitemComponent } from './recipe-books/recipeitem/recipeitem.component';
+import { RecipeComponent } from './recipe-books/recipe/recipe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    EditshoppinglistComponent,
+    HeaderComponent,
+    RecipelistComponent,
+    RecipedetailComponent,
+    RecipeitemComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shopping-lists/editshoppinglist/editshoppinglist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editshoppinglist',
+  templateUrl: './editshoppinglist.component.html',
+  styleUrls: ['./editshoppinglist.component.css']
+})
+export class EditshoppinglistComponent {
+
+}
